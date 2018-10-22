0e19c023977100f6405b255a22053141be723cc7
DESKTOP-1R73VH2\洋 <842556285@qq.com>
2018-10-22 10:21:31 +0800

    wu

--- src/router/index.js
+++ src/router/index.js
@@ -6,11 +6,6 @@ import My from '@/My/My'
 import Comments from '@/Comments/Comments'
 import Coursedetails from '@/Course/Coursedetails'
 import ConfirmationOrders from '@/ConfirmationOrders/ConfirmationOrders'
-// import ConfirmationOrders from '@/ConfirmationOrders/ConfirmationOrders'
-
-import Coursedetails from '@/Course/Coursedetails'
-// >>>>>>> cc49380e0ea8f34c4d49205656daea305ac5dbb0
-// >>>>>>> 482db6919241b535eb3e9482137c2cb868e3a093
 
 Vue.use(Router)
 
